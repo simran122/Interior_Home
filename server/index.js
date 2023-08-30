82297f1f0b3cdbb3bc18de4b9933d5e189d214c9
simran122 <54198569+simran122@users.noreply.github.com>
2023-08-30 19:20:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ import Detail from './models/Detail.js'
 import bodyParser from "body-parser";
 import cors from 'cors';
 const app = express()
-app.use(cors());
+app.use(cors({
+origin:["https://interior-home-sigma.vercel.app/"],
+    methods:["POST"],
+    credentials:true,
+    
+} ));
 app.use(bodyParser.json());
 
 const mongoDB = "mongodb+srv://Simran2612:S8g2S8qFvVqmIXcT@cluster0.njvujv3.mongodb.net/?retryWrites=true&w=majority";
