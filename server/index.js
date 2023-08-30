b7fef3cbbb9069a681d137c19c85bb0ab91299c6
simran122 <54198569+simran122@users.noreply.github.com>
2023-08-30 21:38:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ import cors from 'cors';
 const app = express()
 app.use(cors())
 app.use(bodyParser.json());
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    next();
+    
+})
 
 const mongoDB = "mongodb+srv://Simran2612:S8g2S8qFvVqmIXcT@cluster0.njvujv3.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDB)
