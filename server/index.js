09cb6673479b48044fba07132285565c964a66fa
simran122 <54198569+simran122@users.noreply.github.com>
2023-08-31 00:14:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+   if(req.method==="OPTIONS"{
+     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+       return res.status(200).json({});
+   }
+ 
     next();
     
 })
